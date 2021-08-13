7dd85460d13c332c0bf7730757c0990ebf4dbf4a
rashad <rashad@10.0.2.15>
2021-08-13 13:56:33 +0400

    Service project for Customer data manegment.

---
+++ CustomerService/src/test/java/service/customer/api/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package service.customer.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
