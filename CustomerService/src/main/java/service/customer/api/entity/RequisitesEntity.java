a3d7e503b1254a01592f96c30f1797943d64f87b
marlboro <marlboro1122001@yahoo.com>
2022-01-24 03:25:20 -0600

    Added RequisitesEntity

---
+++ CustomerService/src/main/java/service/customer/api/entity/RequisitesEntity.java
@@ -0,0 +1,83 @@
+package service.customer.api.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Table(name = "Requisites")
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class RequisitesEntity extends BaseEntity {
+    
+    private String bankName;
+    private String bankAddress;
+    private String bankCode;
+    private String bankTaxId;
+    private String bankCorrespondentAccount;
+    private String bankSwift;
+    private String customerName;
+    private String customerAccountNumber;
+    private String currency;
+    
+    public String getBankName() {
+        return bankName;
+    }
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+    public String getBankAddress() {
+        return bankAddress;
+    }
+    public void setBankAddress(String bankAddress) {
+        this.bankAddress = bankAddress;
+    }
+    public String getBankCode() {
+        return bankCode;
+    }
+    public void setBankCode(String bankCode) {
+        this.bankCode = bankCode;
+    }
+    public String getBankTaxId() {
+        return bankTaxId;
+    }
+    public void setBankTaxId(String bankTaxId) {
+        this.bankTaxId = bankTaxId;
+    }
+    public String getBankCorrespondentAccount() {
+        return bankCorrespondentAccount;
+    }
+    public void setBankCorrespondentAccount(String bankCorrespondentAccount) {
+        this.bankCorrespondentAccount = bankCorrespondentAccount;
+    }
+    public String getBankSwift() {
+        return bankSwift;
+    }
+    public void setBankSwift(String bankSwift) {
+        this.bankSwift = bankSwift;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getCustomerAccountNumber() {
+        return customerAccountNumber;
+    }
+    public void setCustomerAccountNumber(String customerAccountNumber) {
+        this.customerAccountNumber = customerAccountNumber;
+    }
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    
+}
