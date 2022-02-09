07699f0fc0fb27d8eff975cd72649fbd20dc0d65
marlboro <marlboro1122001@yahoo.com>
2022-02-09 05:15:46 -0600

    Added Entity fields

--- CustomerService/src/main/java/service/customer/api/entity/RequisitesForForeignCurrencyEntity.java
+++ CustomerService/src/main/java/service/customer/api/entity/RequisitesForForeignCurrencyEntity.java
@@ -2,6 +2,7 @@ package service.customer.api.entity;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -16,14 +17,32 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class RequisitesForForeignCurrencyEntity extends BaseEntity {
     
-    private String bankName;    
+    @Column(name = "bankName", length = 255, nullable = false, unique = false)
+    private String bankName; 
+    
+    @Column(name = "bankCode", length = 255, nullable = false, unique = false)
     private String bankCode;
+
+    @Column(name = "bankTaxId", length = 255, nullable = false, unique = false)
     private String bankTaxId;
+
+    @Column(name = "bankCorrespondentAccount", length = 255, nullable = false, unique = false)
     private String bankCorrespondentAccount;
+
+    @Column(name = "bankSwift", length = 255, nullable = false, unique = false)
     private String bankSwift;
+
+    @Column(name = "bankAddress", length = 255, nullable = false, unique = false)
     private String bankAddress;
+
+    @Column(name = "customerAccountNumber", length = 255, nullable = false, unique = true)
     private String customerAccountNumber;
-    private List <CurrencyEntity> currencyEntities;
+
+
+    @Column(name = "currency", length = 255, nullable = false, unique = false)
+    private List <CurrencyEntity> currency;
+    
+    @Column(name = "correspondentBankEntities", length = 255, nullable = false, unique = false)
     private List<CorrespondentBankEntity> correspondentBankEntities;
     
     public String getBankName() {
@@ -68,11 +87,11 @@ public class RequisitesForForeignCurrencyEntity extends BaseEntity {
     public void setCustomerAccountNumber(String customerAccountNumber) {
         this.customerAccountNumber = customerAccountNumber;
     }
-    public List<CurrencyEntity> getCurrencyEntities() {
-        return currencyEntities;
+    public List<CurrencyEntity> getCurrency() {
+        return currency;
     }
-    public void setCurrencyEntities(List<CurrencyEntity> currencyEntities) {
-        this.currencyEntities = currencyEntities;
+    public void setCurrency(List<CurrencyEntity> currency) {
+        this.currency = currency;
     }
     public List<CorrespondentBankEntity> getCorrespondentBankEntities() {
         return correspondentBankEntities;
--- CustomerService/src/main/java/service/customer/api/entity/RequisitesForLocalCurrencyEntity.java
+++ CustomerService/src/main/java/service/customer/api/entity/RequisitesForLocalCurrencyEntity.java
@@ -2,6 +2,7 @@ package service.customer.api.entity;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -16,13 +17,28 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class RequisitesForLocalCurrencyEntity extends BaseEntity {
     
-    private String bankName;    
+    @Column(name = "bankName", length = 255, nullable = false, unique = false)
+    private String bankName; 
+    
+    @Column(name = "bankCode", length = 255, nullable = false, unique = false)
     private String bankCode;
+
+    @Column(name = "bankTaxId", length = 255, nullable = false, unique = false)
     private String bankTaxId;
+
+    @Column(name = "bankCorrespondentAccount", length = 255, nullable = false, unique = false)
     private String bankCorrespondentAccount;
+
+    @Column(name = "bankSwift", length = 255, nullable = false, unique = false)
     private String bankSwift;
+
+    @Column(name = "bankAddress", length = 255, nullable = false, unique = false)
     private String bankAddress;
+
+    @Column(name = "customerAccountNumber", length = 255, nullable = false, unique = true)
     private String customerAccountNumber;
+
+    @Column(name = "currency", length = 255, nullable = false, unique = false)
     private List <CurrencyEntity> currency;
 
     public String getBankName() {
